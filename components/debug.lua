cb728599d1b5f3da9782a5b36d24ca98392eccea
Renaud Bedard <renaud.bedard@gmail.com>
2010-04-24 15:41:39 -0400

    screw FPS rendering you guise

--- components/debug.lua
+++ components/debug.lua
@@ -7,10 +7,10 @@ local lines = {}
 game.actors.new_generic('debug_component', function ()
   function draw_debug()
     gl.glPushMatrix()
-    gl.glTranslated(2, game.gui.height - 2, 0)
-    gl.glScaled(2, 2, 2)
-    graphics.draw_text(game.resources.font,
-      string.format('fps: %.1f', kernel.get_framerate()))
+    --gl.glTranslated(2, game.gui.height - 2, 0)
+    --gl.glScaled(2, 2, 2)
+    --graphics.draw_text(game.resources.font,
+    --  string.format('fps: %.1f', kernel.get_framerate()))
     gl.glPopMatrix()
   end
 end)
