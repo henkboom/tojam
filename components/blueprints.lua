e30592b5c2424f457eee7d1d0b34c763bc45f97b
Henk Boom <henk@henk.ca>
2010-04-24 15:15:48 -0400

    players have octagonal hitboxes

--- components/blueprints.lua
+++ components/blueprints.lua
@@ -1,22 +1,17 @@
-collision = require 'dokidoki.collision'
-v2 = require 'dokidoki.v2'
+local collision = require 'dokidoki.collision'
+local v2 = require 'dokidoki.v2'
+
+local geom = require 'geom'
 
 character = game.make_blueprint('character',
   {'transform', height=0},
   {'billboard', image = game.resources.sprites.character},
   {'tag', tags={'action'}},
-  {'collider', poly=collision.make_rectangle(16, 16)},
+  {'collider', poly=geom.make_octagon(8)},
   {'character'})
-
-local octagon_hitbox = {}
-for i = 1, 8 do
-  table.insert(octagon_hitbox,
-    v2(math.cos(i*2*math.pi/8), math.sin(i*2*math.pi/8))*16)
-end
-octagon_hitbox = collision.make_polygon(octagon_hitbox)
 
 attack_hitbox = game.make_blueprint('attack_hitbox',
   {'transform'},
   {'tag', tags={'action'}},
-  {'collider', poly=octagon_hitbox},
+  {'collider', poly=geom.make_octagon(16)},
   {'attack_hitbox'})
