04925f37ec1811ff991a22e21ce1c3f1c113fb2f
Henk Boom <henk@henk.ca>
2010-04-25 18:28:26 -0400

    music during voting

--- components/music.lua
+++ components/music.lua
@@ -1,5 +1,6 @@
 local mixer = require 'mixer'
 local channel
+local stop_timer
 
 function play()
   channel = game.resources.music:play(0.5, 0.5, 0)
@@ -7,7 +8,21 @@ end
 
 function stop()
   if channel then
-    mixer.channel_stop(channel)
-    channel = nil
+    mixer.channel_fade_to(channel, 1, 0)
+    stop_timer = 60 + 10
   end
 end
+
+game.actors.new_generic('music_component', function ()
+  function update()
+    if stop_timer then
+      stop_timer = stop_timer - 1
+      if stop_timer == 0 then
+        stop_timer = nil
+        print('stopping')
+        mixer.channel_stop(channel)
+        channel = nil
+      end
+    end
+  end
+end)
