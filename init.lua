58378bc5c9a86c48510a2fb991d3e53ff24f0c3f
Renaud Bedard <renaud.bedard@gmail.com>
2010-04-25 19:51:24 -0400

    fullscreen!

--- init.lua
+++ init.lua
@@ -4,5 +4,14 @@ local kernel = require 'dokidoki.kernel'
 
 local the_game = require 'the_game'
 
+local args = {}
+for _, a in ipairs(arg) do
+  args[a] = true
+end
+
+if not args['--windowed'] then
+  kernel.set_fullscreen(true)
+end
+
 kernel.set_video_mode(800, 600)
 kernel.start_main_loop(the_game.make())
