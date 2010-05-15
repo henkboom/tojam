6d6f4b2af0a8b4be39f3034ee41906fedf5a4422
Renaud Bedard <renaud.bedard@gmail.com>
2010-05-15 18:35:10 -0400

    THE MISSING COMMIT

--- scripts/pickup.lua
+++ scripts/pickup.lua
@@ -1,5 +1,5 @@
 game.collision.add_collider(self, 'player', function (other, correction)
   other.player.attributes.teacup = other.player.attributes.teacup + 1
-	game.rules.register_event(self, "teacup")
+	game.rules.register_event(other, "teacup")
 	self.dead = true
 end)
